781bcc3c8b37c65117c0a779cf1de1469f14ee1f
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2024-09-19 20:23:22 +0000

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ResponsiveAppBar from './components/navbar'
+
+function App() {
+  return (
+    <div className="App">
+      < ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
